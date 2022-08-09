a32ee717665105883e41672d470956b8cd9cd880
marcypotter16 <marcypotter16@gmail.com>
2022-08-09 16:34:11 +0200

    1 file added

---
+++ src/environments/environment.ts
@@ -0,0 +1,19 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA2DSLKT1tL_3V54rJO3ZbsgiYPBSGVU0U",
+
+    authDomain: "kanbanfire-629a4.firebaseapp.com",
+
+    projectId: "kanbanfire-629a4",
+
+    storageBucket: "kanbanfire-629a4.appspot.com",
+
+    messagingSenderId: "615577663419",
+
+    appId: "1:615577663419:web:9e9b80783e32507178b69e",
+
+    measurementId: "G-WMMVMQGD8V"
+
+  }
+};
